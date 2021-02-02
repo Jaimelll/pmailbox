2d30b46e39933eb609723168545476f2c60f18cc
Jaimelll <jaduran0102@gmail.com>
2021-02-02 12:42:35 -0500

    mailbox

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ActionMailer::Base.smtp_settings = {
+    :user_name => 'miapikey',
+    :password => ENV["RAILS_INBOUND_EMAIL_PASSWORD"],
+    :domain => 'jdvizarraga@gmail.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
